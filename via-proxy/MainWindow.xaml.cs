07a97b2cac0bb8d77a2b43a3bd2dc10266d75acb
girorme <rodrigogirorme@gmail.com>
2023-07-05 01:49:37 -0300

    upgrade main layout

--- via-proxy/MainWindow.xaml.cs
+++ via-proxy/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace via_proxy
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Proxy has been upgraded to http...");
+        }
     }
 }
