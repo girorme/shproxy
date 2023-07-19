df8ffaf9fd0b0d360c90ad46dc470fca81296705
girorme <rodrigogirorme@gmail.com>
2023-07-19 10:32:47 -0300

    Add command runner

---
+++ sh-proxy/CommandRunner.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+
+namespace sh_proxy
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+
+    public class CommandRunner
+    {
+        private CancellationTokenSource cancellationTokenSource;
+        private Process process;
+
+        public void StartCommand(string command)
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            var token = cancellationTokenSource.Token;
+
+            process = new Process
+            {
+                StartInfo = new ProcessStartInfo("cmd.exe", "/c " + command)
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                }
+            };
+
+            process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+            process.ErrorDataReceived += (sender, args) => Console.WriteLine(args.Data);
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    process.WaitForExit();
+                }
+                catch (Exception ex)
+                {
+                    // Handle process exit error if needed
+                    Console.WriteLine("Error waiting for process exit: " + ex.Message);
+                }
+            }, token);
+        }
+
+        public void StopCommand()
+        {
+            Process[] processes = Process.GetProcessesByName("sthp");
+
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    process.Kill();
+                    process.WaitForExit(); // Wait for the process to exit gracefully (optional)
+                }
+                catch (Exception ex)
+                {
+                    // Handle process kill error if needed
+                    Console.WriteLine($"Error killing process {process.ProcessName}: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ sh-proxy/ConfigManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sh_proxy
+{
+    class ConfigManager
+    {
+        public string? SshServer { get; set; }
+        public string? SshUsername{ get; set; }
+        public string? SshPassword { get; set; }
+        public uint ProxyPortSocks { get; set; }
+        public uint ProxyPortHttp { get; set; }
+    }
+}
